7d55b3a0c8547ad85b7fd72b9d00d42fd75d86a5
yasirali <yasir.ali@careaxiom.com>
2015-08-06 19:39:06 +0500

    add application class

---
+++ AllianzHealthCareLocator/app/src/main/java/com/yasirali/allianz/healthcarelocator/AllianzApplication.java
@@ -0,0 +1,14 @@
+package com.yasirali.allianz.healthcarelocator;
+
+import android.app.Application;
+
+/**
+ * Created by yasirali on 8/6/15.
+ */
+public class AllianzApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
