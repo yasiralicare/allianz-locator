cc62108f4e63b403fae4b5d4e0e9ab9394efc0e6
yasirali <yasir.ali@careaxiom.com>
2015-08-06 19:46:37 +0500

    test parse sdk using dummy data

--- AllianzHealthCareLocator/app/src/main/java/com/yasirali/allianz/healthcarelocator/AllianzApplication.java
+++ AllianzHealthCareLocator/app/src/main/java/com/yasirali/allianz/healthcarelocator/AllianzApplication.java
@@ -3,6 +3,7 @@ package com.yasirali.allianz.healthcarelocator;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by yasirali on 8/6/15.
@@ -16,5 +17,13 @@ public class AllianzApplication extends Application {
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+
+        testParseSdk();
+    }
+
+    private void testParseSdk(){
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
