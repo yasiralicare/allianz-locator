0bf7e0367e6a86403a5a2621e6aa3d2dac8d41e9
yasirali <yasir.ali@careaxiom.com>
2015-08-06 19:52:46 +0500

    remove test-code, parse tested successfully

--- AllianzHealthCareLocator/app/src/main/java/com/yasirali/allianz/healthcarelocator/AllianzApplication.java
+++ AllianzHealthCareLocator/app/src/main/java/com/yasirali/allianz/healthcarelocator/AllianzApplication.java
@@ -3,7 +3,6 @@ package com.yasirali.allianz.healthcarelocator;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 /**
  * Created by yasirali on 8/6/15.
@@ -18,12 +17,5 @@ public class AllianzApplication extends Application {
 
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
 
-        testParseSdk();
-    }
-
-    private void testParseSdk(){
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 }
